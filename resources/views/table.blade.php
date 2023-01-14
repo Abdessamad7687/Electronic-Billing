5780ecdae737826a319b8dde878fc2940e4ad5a6
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-01-14 20:03:24 +0100

    table

---
+++ resources/views/table.blade.php
@@ -0,0 +1,26 @@
+@extends('layout')
+@section('content')
+
+<div class="container table">
+    <table class="table table-hover">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+       
+    </tbody>
+    </table>
+</div>
+
+@endsection
