0ea2b3a255494fe7ff55137253ca31e1926e2457
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-01-15 20:41:14 +0100

    updating

--- app/Http/Controllers/LangController.php
+++ app/Http/Controllers/LangController.php
@@ -7,5 +7,21 @@ use Illuminate\Http\Request;
 
 class LangController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('lang');
+    }
+  
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+    */
+    public function change(Request $request)
+    {
+        App::setLocale($request->lang);
+        session()->put('locale', $request->lang);
+  
+        return redirect()->back();
+    }
 }
