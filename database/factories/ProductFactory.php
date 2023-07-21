3c1b52786147e3a450849eafa8db635eb3f648e0
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-07-22 00:36:37 +0100

    executing factories

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -17,7 +17,13 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => fake()->name(),
+            'category' => fake()->name(),
+            'price' => fake()->randomFloat(4, 10, 1000),
+            'quantity' => fake()->randomFloat(4, 10, 1000),
+            'storage'=> fake()->randomFloat(4, 10, 1000),
+            'color' => fake()->name(),
+            'total' => fake()->randomFloat(4, 10, 1000),
         ];
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
 
 class ProductSeeder extends Seeder
 {
@@ -14,6 +15,8 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Product::factory()
+            ->count(20)
+            ->create(); 
     }
 }
